43ed1e7019dcdf5b9a060f231bfb8bc6f0d30dea
Anarchy99 <104285103+Buko3654@users.noreply.github.com>
2022-04-27 01:49:20 +0000

    Finished

--- main.py
+++ main.py
@@ -26,4 +26,20 @@ print("Your mission is to find the treasure.")
 #https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
 #Write your code below this line 👇
-
+turn = input("You have reached a fork in the path. Which way would you like to go? L for left or R for right.\n")
+if turn == "L":
+  lake = input("You reach a large lake with an island in the center. Would you like to Swim to the island or Wait for a boat?\n")
+  if lake == "Wait":
+    door = input("You get to the island and see a large house with three doors. One door is Red, one Yellow, and one Blue. Which door would you like to enter?\n")
+    if door == "Red":
+      print("You enter the room that smells a bit smoky. After a few steps the door closes behind you and the room fills with fire. Game Over.")
+    elif door == "Blue":
+      print("You enter a room with open kennels. After a few steps the door closes behind you and you're eaten by beasts. Game Over.")
+    elif door == "Yellow":
+      print("You enter a room that's dim with the exception of a large pillar of light shining down in the center of the room. In the pillar, you see an old chest. You found the treasure! You Win!")
+    else:
+      print("The owner of the house finds you as you're messing with doors and hauls you off to the police for being a thief. Game Over.")
+  else:
+    print("You are attacked by an unbelievably angry trout. Game Over.")
+else:
+  print("You fall into a deep hole and can't climb out. Game Over.")
